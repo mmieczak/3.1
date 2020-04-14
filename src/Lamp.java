1e11998eac24b77b6c3419dd2cb6f46dada23b4a
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-04-14 14:22:14 +0200

    1.2 Added new constructor to Lamp class, changed initialization of two declared objects using new constructor

--- src/Lamp.java
+++ src/Lamp.java
@@ -3,10 +3,15 @@ public class Lamp {
     boolean isTurnedOn;
     int lightingPower;
 
+    Lamp(String t, boolean status, int power) {
+        type = t;
+        isTurnedOn = status;
+        lightingPower = power;
+    }
+
     void showInfo() {
         System.out.println("Lamp type: " + type);
         System.out.println("Is turned on: " + isTurnedOn);
         System.out.println("Lighting power: " + lightingPower);
     }
 }
-
